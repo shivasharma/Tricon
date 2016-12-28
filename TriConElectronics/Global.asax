95a725454e9ff280b6aa93499a6c2fb6df82f703
Shiva Sharma <s.sharma@cipher-sys.com>
2016-12-28 13:22:47 -0500

    Add project files.

---
+++ TriConElectronics/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TriConElectronics.MvcApplication" Language="C#" %>
---
+++ TriConElectronics/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TriConElectronics.Startup))]
+namespace TriConElectronics
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
